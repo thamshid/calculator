8cc7871280c0aed3f8e481fe2b5d3fe708591ddb
thamshid cc <thamshidcc@gmail.com>
2014-10-23 20:55:37 +0530

    + and - buton added

--- calculator/src/calculator/home.java
+++ calculator/src/calculator/home.java
@@ -15,6 +15,10 @@ public class home extends javax.swing.JFrame {
     /**
      * Creates new form NewJFrame
      */
+    int op=0,al,i,f=1;
+    float a,b,r;
+    String sp,k;
+    char ch;
     public home() {
         initComponents();
     }
@@ -40,8 +44,15 @@ public class home extends javax.swing.JFrame {
         jButton8 = new javax.swing.JButton();
         jButton9 = new javax.swing.JButton();
         jButton10 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton15 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        sreen.setEditable(false);
 
         clear.setText("CLEAR");
         clear.addActionListener(new java.awt.event.ActionListener() {
@@ -120,6 +131,41 @@ public class home extends javax.swing.JFrame {
             }
         });
 
+        jButton11.setText(".");
+        jButton11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton11ActionPerformed(evt);
+            }
+        });
+
+        jButton12.setText("0");
+        jButton12.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton12ActionPerformed(evt);
+            }
+        });
+
+        jButton13.setText("=");
+        jButton13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton13ActionPerformed(evt);
+            }
+        });
+
+        jButton14.setText("+");
+        jButton14.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton14ActionPerformed(evt);
+            }
+        });
+
+        jButton15.setText("-");
+        jButton15.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton15ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -137,19 +183,29 @@ public class home extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(jButton3)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton4))
+                        .addComponent(jButton4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton14))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton5)
                         .addGap(18, 18, 18)
                         .addComponent(jButton6)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton7))
+                        .addComponent(jButton7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton15))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton8)
                         .addGap(18, 18, 18)
                         .addComponent(jButton9)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton10)))
+                        .addComponent(jButton10))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton11)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton12)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton13)))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -165,25 +221,33 @@ public class home extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
                     .addComponent(jButton3)
-                    .addComponent(jButton4))
+                    .addComponent(jButton4)
+                    .addComponent(jButton14))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton5)
                     .addComponent(jButton6)
-                    .addComponent(jButton7))
+                    .addComponent(jButton7)
+                    .addComponent(jButton15))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton8)
                     .addComponent(jButton9)
                     .addComponent(jButton10))
-                .addContainerGap(139, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton11)
+                    .addComponent(jButton12)
+                    .addComponent(jButton13))
+                .addContainerGap(98, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearActionPerformed
-sreen.setText("");        // TODO add your handling code here:
+sreen.setText("");    
+op=0;// TODO add your handling code here:
     }//GEN-LAST:event_clearActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -195,55 +259,177 @@ sreen.setText("");        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       String s=sreen.getText();
+       if(f==0){
+           sreen.setText("");
+           f=1;
+       }
+        String s=sreen.getText();
        sreen.setText(s+'1');
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        if(f==0){
+           sreen.setText("");
+           f=1;
+       }
         String s=sreen.getText();
        sreen.setText(s+'2');// TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+      if(f==0){
+           sreen.setText("");
+           f=1;
+       }
       String s=sreen.getText();
        sreen.setText(s+'3');
        // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+     if(f==0){
+           sreen.setText("");
+           f=1;
+       }
      String s=sreen.getText();
        sreen.setText(s+'4');   // TODO add your handling code here:
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+      if(f==0){
+           sreen.setText("");
+           f=1;
+       }
       String s=sreen.getText();
        sreen.setText(s+'5');  // TODO add your handling code here:
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
-     String s=sreen.getText();
+     
+        if(f==0){
+           sreen.setText("");
+           f=1;
+       }
+        String s=sreen.getText();
        sreen.setText(s+'6');   // TODO add your handling code here:
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+if(f==0){
+           sreen.setText("");
+           f=1;
+       }
 String s=sreen.getText();
        sreen.setText(s+'7');        // TODO add your handling code here:
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+if(f==0){
+           sreen.setText("");
+           f=1;
+       }
 String s=sreen.getText();
        sreen.setText(s+'8');        // TODO add your handling code here:
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
+if(f==0){
+           sreen.setText("");
+           f=1;
+       }
 String s=sreen.getText();
        sreen.setText(s+'9');        // TODO add your handling code here:
     }//GEN-LAST:event_jButton10ActionPerformed
 
+    private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
+     String s=sreen.getText();
+       sreen.setText(s+'.');   // TODO add your handling code here:
+    }//GEN-LAST:event_jButton11ActionPerformed
+
+    private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
+       String s=sreen.getText();
+       sreen.setText(s+'0'); // TODO add your handling code here:
+    }//GEN-LAST:event_jButton12ActionPerformed
+
+    private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton13ActionPerformed
+
+    private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
+
+        // TODO add your handling code here:
+        sp=sreen.getText();
+        if(op==0){
+            al=sp.length();
+            a=Float.parseFloat(sp);
+            op=1;f=0;
+          ch='+';
+        }
+        
+        else {
+           
+            f=0;
+            k=String.valueOf(a);
+            al=k.length();
+            b=Float.parseFloat(sp);
+            b=Float.parseFloat(sp);
+            switch(ch){
+                case '+':r=a+b;break;
+                case '-':r=a-b;break;
+                    case '*':r=a*b;break;
+                        case '/':r=a/b;break;
+                
+            }
+            ch='+';
+            a=r;
+             }
+       
+        
+                sreen.setText(String.valueOf(a));
+       
+        
+    }//GEN-LAST:event_jButton14ActionPerformed
+
+    private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
+        // TODO add your handling code here:
+        sp=sreen.getText();
+        if(op==0){
+            al=sp.length();
+            a=Float.parseFloat(sp);
+            op=1;f=0;
+            ch='-';
+          
+        }
+        
+        else {
+           
+            f=0;
+            k=String.valueOf(a);
+            al=k.length();
+            b=Float.parseFloat(sp);
+            b=Float.parseFloat(sp);
+            switch(ch){
+                case '+':r=a+b;break;
+                case '-':r=a-b;break;
+                    case '*':r=a*b;break;
+                        case '/':r=a/b;break;
+                
+            }
+            ch='-';
+            a=r;
+             }
+       
+        
+                sreen.setText(String.valueOf(a));
+       
+        
+    }//GEN-LAST:event_jButton15ActionPerformed
+
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -269,9 +455,10 @@ String s=sreen.getText();
         //</editor-fold>
 
         /* Create and display the form */
+        home h=new home();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new home().setVisible(true);
+                h.setVisible(true);
             }
         });
     }
@@ -280,6 +467,11 @@ String s=sreen.getText();
     private javax.swing.JButton clear;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton15;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
