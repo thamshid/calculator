9c02a5cb5bb777cd79be2f33a816f036d74491fa
thamshid cc <thamshidcc@gmail.com>
2014-10-23 15:45:15 +0530

    number 2 added

--- calculator/src/calculator/home.java
+++ calculator/src/calculator/home.java
@@ -32,6 +32,7 @@ public class home extends javax.swing.JFrame {
         clear = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -56,6 +57,13 @@ public class home extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("2");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -68,7 +76,10 @@ public class home extends javax.swing.JFrame {
                         .addComponent(clear)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jButton1))
-                    .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3)))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -81,7 +92,9 @@ public class home extends javax.swing.JFrame {
                     .addComponent(clear)
                     .addComponent(jButton1))
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
                 .addContainerGap(221, Short.MAX_VALUE))
         );
 
@@ -105,6 +118,11 @@ sreen.setText("");        // TODO add your handling code here:
        sreen.setText(s+'1');
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        String s=sreen.getText();
+       sreen.setText(s+'2');// TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -145,6 +163,7 @@ sreen.setText("");        // TODO add your handling code here:
     private javax.swing.JButton clear;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JTextField sreen;
     // End of variables declaration//GEN-END:variables
 }
